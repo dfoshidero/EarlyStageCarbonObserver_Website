4cbec419d24430a69a2dec2e108b02976d0e90a5
Favour <dfoshidero@outlook.com>
2024-07-29 19:35:45 +0100

    testing new api endpoint

--- src/utils/modelapi.jsx
+++ src/utils/modelapi.jsx
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 // Base URL for the API
-const BASE_URL = "https://e4ihifenad.execute-api.eu-west-2.amazonaws.com/dev";
+const BASE_URL = "https://e4ihifenad.execute-api.eu-west-2.amazonaws.com/prod";
 
 /**
  * Function to call the extract_predict API.
